fa84fa4a67a56bf594632a11ef998348a75cfda9
Sigita Zebrauskaite <sigita.zeb92@gmail.com>
2021-10-12 15:43:08 +0000

    Delete comments

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,18 +9,3 @@ def fizzbuzz(number)
     number
   end
 end
-
-# number = 1
- 
-#   while number <= 100 do
-#     if number % 15 == 0
-#       puts "fizzbuzz"
-#     elsif number % 3 == 0
-#       puts "fizz"
-#     elsif number % 5 == 0
-#       puts "buzz"
-#     else
-#       puts number
-#     end
-#     number += 1
-#   end
