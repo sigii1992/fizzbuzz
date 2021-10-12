3fb1cdb29003a3c14be7efbfe9e6ecc676395960
Mohammed Salah <mohammed_salah@icloud.com>
2021-10-12 15:38:51 +0100

    FizzBuzz solution

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,17 @@
 def fizzbuzz(number)
-  'fizz'
+  number = 1
+
+  while number <= 100 do
+    if number % 15 == 0
+      puts "fizzbuzz"
+    elsif number % 3 == 0
+      puts "fizz"
+    elsif number % 5 == 0
+      puts "buzz"
+    else
+      puts number
+    end
+    number += 1
+  end
 end
+fizzbuzz(3)
